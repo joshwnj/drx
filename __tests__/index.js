ce6eb2d035170d50155787043093da82abf687c9
Josh Johnston <josh@x-team.com>
2017-09-01 12:41:39 +1000

    don't need to select children in these cases

--- __tests__/index.js
+++ __tests__/index.js
@@ -46,7 +46,6 @@ describe('Div with a conditional class', () => {
 describe('Basic conditional rendering', () => {
   const Component = x.div()
     .renderIf('active')
-    .props(({ children }) => ({ children }))
 
   it('with the prop flag', () => {
     expect(
@@ -64,7 +63,6 @@ describe('Basic conditional rendering', () => {
 describe('Conditional rendering by function', () => {
   const Component = x.div()
     .renderIf(p => p.a + p.b > 10)
-    .props(({ children }) => ({ children }))
 
   it('with sum of props greater than 10', () => {
     expect(
